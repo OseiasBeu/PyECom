d89c89d0ab6c82e648501fd5eda04114d010d896
oseiasbeu <oseiasbeu@outlook.com>
2020-08-23 18:02:04 -0300

    Fazendo login por email também

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -156,6 +156,10 @@ DEFAULT_FROM_EMAIL = 'yehiorGlobal@gmail.com'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend',
+)
 
 
 
