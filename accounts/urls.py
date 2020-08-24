ed38ef34fd5e2e93f1d1effa3137aea10743451f
oseiasbeu <oseiasbeu@outlook.com>
2020-08-23 21:46:26 -0300

    Test alteracao de dados de usuario

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^registro/$', views.register, name='register')
+    url(r'^$', views.index, name='index'),
+    url(r'^registro/$', views.register, name='register'),
+    url(r'^alterar-dados/$', views.update_user, name='update_user'),
+    url(r'^alterar-senha/$', views.update_password, name='update_password'),
+
 ]
