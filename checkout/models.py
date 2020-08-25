8b26c452e562d320f3ca356d1da2f3fd64c04832
oseiasbeu <oseiasbeu@outlook.com>
2020-08-25 19:27:09 -0300

    Adicionando item no carrinho de compras

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,12 @@
 from django.db import models
 
 
+class CartItemManager(models.Manager):
+
+    def add_item(self, cart_key, product):
+        pass
+
+
 class CartItem(models.Model):
 
     cart_key = models.CharField(
@@ -12,9 +18,12 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField('Quantidade', default=1)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
 
+    objects = CartItemManager()
+
     class Meta:
         verbose_name = 'Item do Carrinho'
         verbose_name_plural = 'Itens dos Carrinhos'
+        unique_together = (('cart_key','product'),)
 
     def __str__(self):
         return '{} [{}]'.format(self.product, self.quantity)
