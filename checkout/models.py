feb3b54800dcbfaf9c0b1fc6764e83f8c7a0b17e
oseiasbeu <oseiasbeu@outlook.com>
2020-08-28 15:54:47 -0300

    Criando pagina: Meus pedidos

--- checkout/models.py
+++ checkout/models.py
@@ -3,19 +3,22 @@
 from django.db import models
 from django.conf import settings
 
+from catalog.models import Product
+
 
 class CartItemManager(models.Manager):
 
     def add_item(self, cart_key, product):
         if self.filter(cart_key=cart_key, product=product).exists():
-            created = False 
-            cart_item = self.get(cart_key=cart_key,product=product)
+            created = False
+            cart_item = self.get(cart_key=cart_key, product=product)
             cart_item.quantity = cart_item.quantity + 1
             cart_item.save()
-        else: 
+        else:
             created = True
-            cart_item = CartItem.objects.create(cart_key=cart_key,product=product, price=product.price)
-            
+            cart_item = CartItem.objects.create(
+                cart_key=cart_key, product=product, price=product.price
+            )
         return cart_item, created
 
 
@@ -33,71 +36,77 @@ class CartItem(models.Model):
     class Meta:
         verbose_name = 'Item do Carrinho'
         verbose_name_plural = 'Itens dos Carrinhos'
-        unique_together = (('cart_key','product'),)
+        unique_together = (('cart_key', 'product'),)
 
     def __str__(self):
         return '{} [{}]'.format(self.product, self.quantity)
 
+
 class OrderManager(models.Manager):
+
     def create_order(self, user, cart_items):
         order = self.create(user=user)
         for cart_item in cart_items:
             order_item = OrderItem.objects.create(
-                order=order, quantity=cart_item.quantity,product=cart_item.product,
+                order=order, quantity=cart_item.quantity, product=cart_item.product,
                 price=cart_item.price
             )
         return order
 
 
-
 class Order(models.Model):
 
     STATUS_CHOICES = (
-        (0,'Aguardando Pagamento'),
-        (1, 'Compra concluída'),
+        (0, 'Aguardando Pagamento'),
+        (1, 'Concluída'),
         (2, 'Cancelada'),
     )
 
     PAYMENT_OPTION_CHOICES = (
-        ('deposit','Depósito'),
-        ('pagseguro','PagSeguro'),
-        ('paypal','Paypal'),
-
+        ('deposit', 'Depósito'),
+        ('pagseguro', 'PagSeguro'),
+        ('paypal', 'Paypal'),
     )
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário')
     status = models.IntegerField(
         'Situação', choices=STATUS_CHOICES, default=0, blank=True
-        )
+    )
     payment_option = models.CharField(
-        'Opção de Pagamento',choices=PAYMENT_OPTION_CHOICES,max_length=20,
+        'Opção de Pagamento', choices=PAYMENT_OPTION_CHOICES, max_length=20,
         default='deposit'
     )
 
     created = models.DateTimeField('Criado em', auto_now_add=True)
-    modified = models.DateTimeField('Modificado em',auto_now=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
     objects = OrderManager()
 
     class Meta:
         verbose_name = 'Pedido'
         verbose_name_plural = 'Pedidos'
+
     def __str__(self):
         return 'Pedido #{}'.format(self.pk)
 
+    def products(self):
+        products_ids = self.items.values_list('product')
+        return Product.objects.filter(pk__in=products_ids)
+
 
 class OrderItem(models.Model):
+
     order = models.ForeignKey(Order, verbose_name='Pedido', related_name='items')
     product = models.ForeignKey('catalog.Product', verbose_name='Produto')
     quantity = models.PositiveIntegerField('Quantidade', default=1)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
 
-    class Meta():
+    class Meta:
         verbose_name = 'Item do pedido'
         verbose_name_plural = 'Itens dos pedidos'
 
     def __str__(self):
         return '[{}] {}'.format(self.order, self.product)
-    
-
 
 
 def post_save_cart_item(instance, **kwargs):
